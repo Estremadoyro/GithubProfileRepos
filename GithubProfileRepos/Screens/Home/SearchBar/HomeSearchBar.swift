0008c795e2ef69fee8432630fe1d82591aaccd47
Leonardo <lmestremadoyro@gmail.com>
2022-04-12 22:09:01 -0500

    disposing searchbar when not focusing on it

--- GithubProfileRepos/Screens/Home/SearchBar/HomeSearchBar.swift
+++ GithubProfileRepos/Screens/Home/SearchBar/HomeSearchBar.swift
@@ -5,13 +5,20 @@
 //  Created by Leonardo  on 11/04/22.
 //
 
+import RxSwift
 import UIKit
 
 final class HomeSearchBar: UISearchController {
+  fileprivate var searchBarDisposable: Disposable?
+  fileprivate let disposeBag = CompositeDisposable()
+
   init() {
+    print("Inited search controller")
     super.init(searchResultsController: SearchResultsVC())
     configureSearchBar()
   }
+
+  deinit { print("\(self) deinited ") }
 
   @available(*, unavailable)
   required init?(coder: NSCoder) {
@@ -25,6 +32,17 @@ extension HomeSearchBar {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    configureBindings()
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    let disposables: [Disposable?] = [searchBarDisposable]
+    disposeSequences(disposables)
+  }
 }
 
 private extension HomeSearchBar {
@@ -34,7 +52,24 @@ private extension HomeSearchBar {
   }
 }
 
-extension HomeSearchBar: UISearchControllerDelegate {
-  func didPresentSearchController(_ searchController: UISearchController) {
+private extension HomeSearchBar {
+  func configureBindings() {
+    bindSearchBar()
+  }
+
+  func disposeSequences(_ disposables: [Disposable?]) {
+    disposables.forEach { disposable in
+      if let disposable = disposable, let disposableKey = disposeBag.insert(disposable) {
+        disposeBag.remove(for: disposableKey)
+      }
+    }
+  }
+}
+
+private extension HomeSearchBar {
+  func bindSearchBar() {
+    searchBarDisposable = searchBar.rx.value.subscribe(onNext: { value in
+      print("value: \(value ?? "")")
+    }, onDisposed: { print("Searchbar disposed") })
   }
 }
--- GithubProfileRepos/Screens/Home/SearchBar/SearchResults/SearchResultsVC.swift
+++ GithubProfileRepos/Screens/Home/SearchBar/SearchResults/SearchResultsVC.swift
@@ -15,6 +15,8 @@ final class SearchResultsVC: UIViewController {
     super.init(nibName: Nibs.searchResultsView, bundle: Bundle.main)
   }
 
+  deinit { print("\(self) deinited") }
+
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
